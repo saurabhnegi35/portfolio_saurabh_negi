f27cabf23f27a0a858a03fc97ae9d47f5154a7ce
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-04-18 11:27:49 +0530

    Added GSAP Animation to Projects Section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+
+    projects.forEach((project, index) => {
+      gsap.fromTo(
+        project,
+        { y: 80, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: project,
+            // start: "top bottom-=100",
+            // toggleActions: "play none none reverse",
+            start: "top bottom",
+            end: "top center",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
-    <div id="project" className="app-showcase px-10">
+    <div ref={sectionRef} id="project" className="app-showcase px-10">
       <div className="w-full">
         <div className="showcaselayout">
-          <div className="first-project-wrapper">
+          <div ref={project1Ref} className="first-project-wrapper">
             <div className="image-wrapper">
               <img src="/images/project1.png" alt="Uber-Clone" />
             </div>
@@ -22,14 +67,14 @@ const Projects = () => {
           </div>
 
           <div className="project-list-wrapper overflow-hidden">
-            <div className="project">
+            <div ref={project2Ref} className="project">
               <div className="image-wrapper bg-[#ffefdb]">
                 <img src="/images/project2.png" alt="Zentry-Clone" />
               </div>
               <h2>Zentry - Explore the world of Gaming</h2>
             </div>
 
-            <div className="project">
+            <div ref={project3Ref} className="project">
               <div className="image-wrapper bg-[#ffe7eb]">
                 <img src="/images/project3.png" alt="TechMate" />
               </div>
