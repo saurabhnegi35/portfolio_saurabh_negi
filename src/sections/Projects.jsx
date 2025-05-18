4445c6c4c37d1ed3e4617677d0682e52093725bd
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-05-18 11:20:07 +0530

    Added Project and updated links and favicon

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -47,46 +47,66 @@ const Projects = () => {
   }, []);
 
   return (
-    <div ref={sectionRef} id="projects" className="app-showcase md:px-50 px-5">
-      <div className="w-full">
-        <div className="showcaselayout">
-          <div ref={project1Ref} href="" className="first-project-wrapper">
-            <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Uber-Clone" />
-            </div>
-            <div className="text-content">
-              <h2>
-                On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                Called Uber-Clone
-              </h2>
-              <p className="text-white-50 md:text-xl">
-                A Web-App build with React, NodeJs, MongoDB etc for fast,
-                suer-friendly experience.
-              </p>
-            </div>
-          </div>
-
-          <div className="project-list-wrapper overflow-hidden">
-            <div
-              ref={project2Ref}
-              href="https://zentry-clone-frontend.vercel.app/"
-              className="project"
+    <div ref={sectionRef} id="projects">
+      <div className="app-showcase md:px-50 px-5">
+        <div className="w-full">
+          <div className="showcaselayout">
+            <a
+              ref={project1Ref}
+              href="https://launchpad-pearl.vercel.app/"
+              className="first-project-wrapper"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <div className="image-wrapper bg-[#ffefdb]">
-                <img src="/images/project2.png" alt="Zentry-Clone" />
+              <div className="image-wrapper">
+                <img src="/images/launchpad.png" alt="launchpad" />
               </div>
-              <h2>Zentry - Explore the world of Gaming</h2>
-            </div>
+              <div className="text-content">
+                <h2>Pitch Your Startup, Connect with Entrepreneurs</h2>
+                <p className="text-white-50 md:text-xl">
+                  Submit Ideas, Vote on Pitches, and Get noticed in Virtual
+                  Competitions
+                </p>
+              </div>
+            </a>
 
-            <div ref={project3Ref} className="project">
-              <div className="image-wrapper bg-[#ffe7eb]">
-                <img src="/images/project3.png" alt="TechMate" />
+            <div className="project-list-wrapper overflow-hidden">
+              <a
+                ref={project2Ref}
+                className="project"
+                href="https://zentry-clone-frontend.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="image-wrapper bg-[#ffefdb]">
+                  <img src="/images/project2.png" alt="Zentry-Clone" />
+                </div>
+                <h2>Zentry - Explore the world of Gaming</h2>
+              </a>
+
+              <div ref={project3Ref} className="project">
+                <div className="image-wrapper bg-[#ffe7eb]">
+                  <img src="/images/project1.png" alt="Uber-Clone" />
+                </div>
+                <h2>
+                  On-Demand Rides Made Simple with a Powerful, User-Friendly App
+                  Called Uber-Clone
+                </h2>
               </div>
-              <h2>TechMate - Find your Developer Buddy</h2>
             </div>
           </div>
         </div>
       </div>
+      <p className="md:px-50 px-10 mx-auto text-2xl">
+        All Projects :-{" "}
+        <a
+          href="https://github.com/saurabhnegi35"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @saurabhnegi35
+        </a>
+      </p>
     </div>
   );
 };
